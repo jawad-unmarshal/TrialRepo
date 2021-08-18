e5b721e8a98f9a1d78d8c4e0cb5145a45461b9a9
Jawad Ahmed Khan <88373522+jawad-unmarshal@users.noreply.github.com>
2021-08-18 18:27:07 +0530

    Commit from GoLand to verify if signed

--- GO-Stuff/src/Channel-Stuff.go
+++ GO-Stuff/src/Channel-Stuff.go
@@ -8,16 +8,16 @@ import (
 var finalAns float64
 
 func fact(n float64, c chan float64) {
-	f := 1.;
-	for i:= 2.; i<=n;i++ {
+	f := 1.
+	for i := 2.; i <= n; i++ {
 		f *= i
 
 	}
 	c <- f
 }
-func anodaFact(n float64)  {
-	f := 1.;
-	for i:= 2.; i<=n;i++ {
+func anodaFact(n float64) {
+	f := 1.
+	for i := 2.; i <= n; i++ {
 		f *= i
 
 	}
@@ -27,20 +27,22 @@ func anodaFact(n float64)  {
 
 func main() {
 	fmt.Println()
-	fmt.Println(strings.Repeat("#-##-#",10))
+	fmt.Println(strings.Repeat("#-##-#", 10))
 	fmt.Println("Without Channels")
-	// ch := make(chan float64)
+	ch := make(chan float64)
 	// defer close(ch)
 
-	for i:= 1.; i<=50;i++ {
+	for i := 1.; i <= 50; i++ {
 		// go fact(i,ch)
 		go anodaFact(i)
+		go fact(i, ch)
 
-		// f := <-ch
+		f := <-ch
 		// fmt.Println("Factorial of",i,"=",f)
 
-		fmt.Println("Factorial of",i,"=",finalAns)
+		fmt.Println("Factorial of", i, "=", finalAns)
+		fmt.Println("Using channels, the factorial of", i, "is:", f)
+		//	Minor change to test out a signed commit
 	}
-
 
 }
